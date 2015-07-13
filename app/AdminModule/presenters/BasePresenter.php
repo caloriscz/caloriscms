5f9d2730faafe75f4aef19a44856f0b73fc13033
Petr Karasek <caloris@caloris.cz>
2015-07-13 15:45:39 +0200

    APP_DIR constant added

--- app/AdminModule/presenters/BasePresenter.php
+++ app/AdminModule/presenters/BasePresenter.php
@@ -16,5 +16,18 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter {
     public function __construct(\Nette\Database\Context $database) {
         $this->database = $database;
     }
+	
+	    protected function startup()
+    {
+        parent::startup();
+
+        if ($this->user->isLoggedIn()) {
+            $this->template->isLoggedIn = TRUE;
+
+            $this->template->member = $this->database->table("users")->get($this->user->getId());
+        }
+
+        $this->template->appDir = APP_DIR;
+    }
 
 }
--- www/index.php
+++ www/index.php
@@ -3,6 +3,8 @@
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require '.maintenance.php';
 
+define('APP_DIR', dirname(__FILE__));
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getService('application')->run();
