96f7cf7f9fb95fcd96528bdf78703f1a527c1fd3
Petr Karasek <caloris@caloris.cz>
2015-07-20 21:22:09 +0200

    File Manager update

---
+++ app/AdminModule/presenters/FilesPresenter.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Nette,
+    App\Model;
+
+/**
+ * Homepage presenter.
+ */
+class FilesPresenter extends BasePresenter
+{
+
+    protected function startup()
+    {
+        parent::startup();
+
+        if (!$this->user->isLoggedIn()) {
+            if ($this->user->logoutReason === Nette\Security\IUserStorage::INACTIVITY) {
+                $this->flashMessage('You have been signed out due to inactivity. Please sign in again.');
+            }
+            $this->redirect('Sign:in', array('backlink' => $this->storeRequest()));
+        }
+    }
+
+    /**
+     * Upload file form
+     */
+    function createComponentUploadForm()
+    {
+        $form = new \Nette\Forms\BootstrapUIForm;
+        $form->setTranslator($this->translator);
+        $form->getElementPrototype()->class = "form-horizontal";
+        $form->getElementPrototype()->role = 'form';
+        $form->getElementPrototype()->autocomplete = 'off';
+
+        $form->addHidden('id', filter_input(INPUT_GET, "id"));
+        $form->addUpload('the_file', 'admin.files.choosefile');
+        $form->addSubmit('submitm', 'admin.files.upload');
+
+        $form->onSuccess[] = $this->uploadFormSucceeded;
+        return $form;
+    }
+
+    /**
+     * Upload file
+     */
+    function uploadFormSucceeded(\Nette\Forms\BootstrapUIForm $form)
+    {
+        $filePath = _CALSET_PATHS_BASE . '/caloris_www/binaries/app/' . $filename;
+        $filePathThumb = _CALSET_PATHS_BASE . '/caloris_www/binaries/app/t200_' . $filename;
+
+        if (file_exists($filePath)) {
+            $msg = 'Soubor již existuje';
+        }
+
+        copy($_FILES["the_file"]["tmp_name"], $filePath);
+        chmod($filePath, 0755);
+
+        return $params;
+    }
+
+    /**
+     * Delete files
+     */
+    function deleteFormSucceeded()
+    {
+        \Caloris\IO::remove(_CALSET_PATHS_BASE . '/www/images/' . $_POST["filename"]);
+        \Caloris\IO::remove(_CALSET_PATHS_BASE . '/www/images/t200_' . $_POST["filename"]);
+
+        $this->redirect("Files:default");
+    }
+
+    public function renderDefault()
+    {
+        //$dir = ;
+        $this->template->files = \Nette\Utils\Finder::findFiles('')->in(APP_DIR . "/files");
+    }
+
+}
