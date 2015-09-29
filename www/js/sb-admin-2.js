1fc185277073a5083042d37199dee08565f4d6f3
Petr Karasek <caloris@caloris.cz>
2015-09-29 16:21:38 +0200

    Style updates

--- www/js/sb-admin-2.js
+++ www/js/sb-admin-2.js
@@ -107,7 +107,25 @@ $(document).ready(function () {
     jQuery.browser = browser;
 
     // Autocmplete
-    var groupkey = $("input[name='paramkey']").attr("data-params").split(";");
-    $("input[name='paramkey']").autocomplete(groupkey);
+    if ($("input[name='paramkey']").length > 0) {
+        var groupkey = $("input[name='paramkey']").attr("data-params").split(";");
+        $("input[name='paramkey']").autocomplete(groupkey);
+    }
+
+    //Tree View
+    $(function () {
+        $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+        $('.tree li.parent_li > span').on('click', function (e) {
+            var children = $(this).parent('li.parent_li').find(' > ul > li');
+            if (children.is(":visible")) {
+                children.hide('fast');
+                $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+            } else {
+                children.show('fast');
+                $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+            }
+            e.stopPropagation();
+        });
+    });
 
 });
