65e03c809d5ea7f5e66f947c452b6808bc85dfee
Petr Karasek <caloris@caloris.cz>
2016-08-31 10:59:30 +0200

    Multilanguage suport for MenuControl

--- app/components/Menus/MenuControl.php
+++ app/components/Menus/MenuControl.php
@@ -14,17 +14,26 @@ class MenuControl extends Control
         $this->database = $database;
     }
 
-    public function render($id = null, $style = 'sidemenu')
+    public function render($id = null, $style = 'sidemenu', $controlName = null)
     {
         $template = $this->template;
 
-        $template->setFile(__DIR__ . '/MenuControl.latte');
+        if ($controlName == null) {
+            $controlName = 'MenuControl';
+        }
+
+        if ($this->presenter->translator->getLocale() != $this->presenter->translator->getDefaultLocale()) {
+            $template->langSuffix = '_' . $this->presenter->translator->getLocale();
+            $template->langPrefix = '/' . $this->presenter->translator->getLocale();
+        }
+
+        $template->setFile(__DIR__ . '/' . $controlName . '.latte');
 
         $template->active = strtok($_SERVER["REQUEST_URI"], '?');
 
         $template->id = $id;
         $template->style = $style;
-        $template->categories = $this->database->table('menu')->where('parent_id', $id);
+        $template->categories = $this->database->table('menu')->where('parent_id', $id)->order('sorted DESC');
         $template->render();
     }
 
