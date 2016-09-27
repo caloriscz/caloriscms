fe12e9f378b360abda46ef7621aad0f297412e76
Petr Karasek <caloris@caloris.cz>
2016-09-27 20:09:42 +0200

    Edit carousel form moved to component

--- app/AdminModule/presenters/AppearancePresenter.php
+++ app/AdminModule/presenters/AppearancePresenter.php
@@ -11,67 +11,10 @@ use Nette,
 class AppearancePresenter extends BasePresenter
 {
 
-    /**
-     * Edit category
-     */
-    protected function createComponentCarouselEditForm()
+    protected function createComponentEditCarousel()
     {
-        $carousel = $this->database->table("carousel")->get($this->getParameter("id"));
-
-        $form = $this->baseFormFactory->createUI();
-        $form->addHidden('carousel_id');
-        $form->addText('title', 'dictionary.main.Title');
-        $form->addTextArea('description', 'dictionary.main.Description')
-            ->setAttribute("class", "form-control")
-            ->setAttribute("style", "max-height: 150px;");
-        $form->addText('uri', 'dictionary.main.URL');
-        $form->addCheckbox("visible", "dictionary.main.Show");
-        $form->addUpload('the_file', 'dictionary.main.Icon');
-        $form->addSubmit('submitm', 'dictionary.main.Save');
-
-
-        $arr = array(
-            "carousel_id" => $carousel->id,
-            "title" => $carousel->title,
-            "description" => $carousel->description,
-            "visible" => $carousel->visible,
-            "uri" => $carousel->uri,
-        );
-
-        $form->setDefaults(array_filter($arr));
-
-        $form->onSuccess[] = $this->carouselEditFormSucceeded;
-        return $form;
-    }
-
-    public function carouselEditFormSucceeded(\Nette\Forms\BootstrapUIForm $form)
-    {
-        $image = $form->values->the_file->name;
-
-        $arr = array(
-            "title" => $form->values->title,
-            "description" => $form->values->description,
-            "uri" => $form->values->uri,
-            "visible" => $form->values->visible,
-        );
-
-        if ($form->values->the_file->error == 0) {
-			$arr['image'] = $image;
-			
-            if (file_exists(APP_DIR . "/images/carousel/" . $image)) {
-
-                \App\Model\IO::remove(APP_DIR . "/images/carousel/" . $image);
-                \App\Model\IO::upload(APP_DIR . "/images/carousel/", $image, 0644);
-            } else {
-                \App\Model\IO::upload(APP_DIR . "/images/carousel/", $image, 0644);
-            }
-        }
-
-        $this->database->table("carousel")->get($form->values->carousel_id)
-            ->update($arr);
-
-
-        $this->redirect(":Admin:Appearance:carouselDetail", array("carousel_id" => $form->values->carousel_id));
+        $control = new \Caloriscz\Appearance\EditCarouselControl($this->database);
+        return $control;
     }
 
     /**
