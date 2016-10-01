ba0d2da9e3d4a556d1411663571f8d734a69a3e1
Petr Karasek <caloris@caloris.cz>
2016-10-01 21:07:56 +0200

    Missing settings and typos fixed

--- app/components/Menus/Admin/MainMenuControl.php
+++ app/components/Menus/Admin/MainMenuControl.php
@@ -7,7 +7,7 @@ use Nette\Application\UI\Control;
 class MainMenuControl extends Control
 {
 
-    /** @var Nette\Database\Context */
+    /** @var \Nette\Database\Context */
     public $database;
 
     public function __construct(\Nette\Database\Context $database)
@@ -21,6 +21,7 @@ class MainMenuControl extends Control
         $template = $this->template;
 
         $template->member = $this->presenter->template->member;
+        $template->settings = $this->presenter->template->settings;
         $template->database = $this->database;
 
         $template->setFile(__DIR__ . '/MainMenuControl.latte');
