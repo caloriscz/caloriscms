0af3e9569cc00bf5be04b2123889a746473335dc
Petr Karasek <caloris@caloris.cz>
2016-10-31 17:53:27 +0100

    Admin homepage moved to Dashboard component

--- app/AdminModule/presenters/HomepagePresenter.php
+++ app/AdminModule/presenters/HomepagePresenter.php
@@ -10,39 +10,13 @@ use Nette,
  */
 class HomepagePresenter extends BasePresenter
 {
+    /** @var \Caloriscz\Navigation\IDashboardControlFactory @inject */
+    public $dashboardFactory;
 
-    protected function startup()
+    protected function createComponentDashboard()
     {
-        parent::startup();
-
-        if ($this->template->settings['store:enabled']) {
-            $this->template->orders = $this->database->table("orders")->where("orders_states_id NOT", null)->count();
-            $this->template->carts = $this->database->table("orders")->where("orders_states_id", null)->count();
-
-            $stats = new Model\Store\Statistics($this->database);
-
-            $statsParam = $this->getParameter('stats');
-
-            if ($statsParam == 'td') {
-                $arrTurnover = $stats->getData('d', 'price');
-                $this->template->statsLabel = 'Obraty - denní';
-            } elseif ($statsParam == 'am') {
-                $arrTurnover = $stats->getData('m', 'amount');
-                $this->template->statsLabel = 'Počty objednávek - měsíční';
-            } elseif ($statsParam == 'ad') {
-                $arrTurnover = $stats->getData('d', 'amount');
-                $this->template->statsLabel = 'Počty objednávek - denní';
-            } else {
-                $arrTurnover = $stats->getData('m', 'price');
-                $this->template->statsLabel = 'Obraty - denní';
-            }
-
-            $this->template->turnoverDays = $stats->convertKeysToString($arrTurnover);
-
-            if (is_array($arrTurnover)) {
-                $this->template->turnoverValues = implode(",", $arrTurnover);
-            }
-        }
+        $control = $this->dashboardFactory->create();
+        return $control;
     }
 
     protected function createComponentElfinder()
