9a27438f527e4ed7f96894890e22949d6eed0ae4
Petr Karasek <caloris@caloris.cz>
2016-11-01 15:08:56 +0100

    Travis file updated

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,5 @@ $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__ . '/../app')
     ->register();
-$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
 $configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
 return $configurator->createContainer();
