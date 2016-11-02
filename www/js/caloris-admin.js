4aed996f0b3f17ee8abc9dfa90c076544f2b6ffd
Petr Karasek <caloris@caloris.cz>
2016-11-02 14:48:21 +0100

    Autocomplete function fixed

--- www/js/caloris-admin.js
+++ www/js/caloris-admin.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $('#myModal').on('shown.bs.modal', function () {
         $('#frm-insertForm-title').focus();
     })
+
 });
+
+/*Parametres autocomplete */
+if ($("input[name='paramkey']").length > 0) {
+    var groupkey = $("input[name='paramkey']").attr("data-params").split(";");
+    $("input[name='paramkey']").autocomplete({source: groupkey});
+}
