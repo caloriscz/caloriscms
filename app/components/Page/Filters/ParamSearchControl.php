e76bd00403d65b45086fc516e3a100654c80df47
Petr Karasek <caloris@caloris.cz>
2016-12-03 09:20:38 +0100

    Replace values, user classes and user form settings added[ci skip]

--- app/components/Page/Filters/ParamSearchControl.php
+++ app/components/Page/Filters/ParamSearchControl.php
@@ -14,6 +14,12 @@ class ParamSearchControl extends Control
         $this->database = $database;
     }
 
+    protected function createComponentPageSlug()
+    {
+        $control = new \Caloriscz\Page\PageSlugControl($this->database);
+        return $control;
+    }
+
     protected function createComponentSearchForm()
     {
         $form = new \Nette\Forms\BootstrapPHForm();
@@ -29,13 +35,15 @@ class ParamSearchControl extends Control
         $form->addHidden("priceFrom");
         $form->addHidden("priceTo");
         $form->addHidden("brand");
+        $form->addHidden("category");
 
         $form->setDefaults(array(
             "idr" => $this->presenter->getParameter("id"),
             "src" => $this->presenter->getParameter("src"),
             "priceFrom" => $this->presenter->getParameter("priceFrom"),
-            "PriceTo" => $this->presenter->getParameter("priceTo"),
+            "priceTo" => $this->presenter->getParameter("priceTo"),
             "brand" => $this->presenter->getParameter("brand"),
+            "category" => $this->presenter->getParameter("page_id"),
         ));
 
         $form->addSubmit('submitm', 'dictionary.main.Save')
@@ -49,28 +57,122 @@ class ParamSearchControl extends Control
     public function searchFormSucceeded(\Nette\Forms\BootstrapPHForm $form)
     {
         $values = $form->getHttpData($form::DATA_TEXT); // get value from html input
-        unset($values["do"], $values["action"], $values["idr"]);
+        unset($values["do"], $values["action"], $values["idr"], $values["locale"]
+            , $values["submitm"], $values["minsNow"], $values["maxNow"], $values["maxANow"], $values["category"]);
 
-        $this->presenter->redirect(":Front:Catalogue:default", $values);
+        foreach ($values as $pmKey => $pmValue) {
+            $pmKeyArr = explode("_", $pmKey);
+
+            if (substr($pmKey, 0, 3) == 'pm_') {
+                if ($pmKeyArr[1] == $pmKeyLast) {
+                    $pmValues = $pmValues . "*" . $pmValue;
+                } else {
+                    $pmValues = $pmValue;
+                }
+
+                $pmKeyLast = $pmKeyArr[1];
+
+                if (isset($pmKeyArr[3])) {
+                    $pmType = "_" . $pmKeyArr[3];
+                } else {
+                    $pmType = null;
+                }
+
+                $pmArray["pm_" . $pmKeyArr[1] . $pmType] = $pmValues;
+            }
+        }
+
+        /* Unset params pm_ from the $values */
+        foreach ($values as $key => $value) {
+            if (strpos($key, 'pm_') !== false) {
+                unset($values[$key]);
+            }
+        }
+
+        /*
+                echo 'testing:<br>';
+                echo dump($pmArray);
+                echo http_build_query($pmArray);
+                parse_str(http_build_query($pmArray), $test);
+                echo "test query:<br>";
+                dump($test);
+        */
+        //dump(array_merge($values, $pmArray));
+
+        $category = $form->values->category;
+        $catalogue = $this->database->table("pages")->get($category);
+
+        $this->presenter->redirectUrl("/" . $catalogue->slug . "?" . http_build_query(array_merge(array_filter($values), array_filter($pmArray))));
+    }
+
+    protected function createComponentPageTitle()
+    {
+        $control = new \Caloriscz\Page\PageTitleControl($this->database);
+        return $control;
     }
 
     public function render()
     {
         $template = $this->template;
         $template->database = $this->database;
+        $template->selected = $this->getParams();
+
+        $template->qs = array_filter($this->presenter->getParameters());
+        unset($template->qs["action"], $template->qs["page_id"]);
 
         if ($this->presenter->translator->getLocale() != $this->presenter->translator->getDefaultLocale()) {
             $template->langSuffix = '_' . $this->presenter->translator->getLocale();
         }
 
-        $params = $this->database->table("param")->where(array(
-            ":param_categories.pages_id" => $this->presenter->getParameter("page_id"),
-        ));
+        if ($this->presenter->getParameter("page_id") == 20) {
+            $params = $this->database->table("param")->where(array(
+                "ignore_front" => 0,
+            ));
+
+        } else {
+            $params = $this->database->table("param")->where(array(
+                ":param_categories.pages_id" => $this->presenter->getParameter("page_id"),
+                "ignore_front" => 0,
+            ))->order("sorted");
+        }
+
         $template->params = $params;
+        $template->page = $this->presenter->template->page;
 
         $template->setFile(__DIR__ . '/ParamSearchControl.latte');
 
         $template->render();
     }
 
+    /**
+     * Function for getting parrams into array
+     */
+    function getParams()
+    {
+        $values = $this->presenter->getParameters(true);
+
+        foreach ($values as $pmKey => $pmValue) {
+            $pmKeyArr = explode("_", $pmKey);
+
+            if (substr($pmKey, 0, 3) == 'pm_') {
+                if ($pmKeyArr[1] == $pmKeyLast) {
+                    $pmValues = $pmValues . "*" . $pmValue;
+                } else {
+                    $pmValues = $pmValue;
+                }
+
+                $pmKeyLast = $pmKeyArr[1];
+
+                if (isset($pmKeyArr[3])) {
+                    $pmType = "_" . $pmKeyArr[3];
+                } else {
+                    $pmType = null;
+                }
+
+                $pmArray["pm_" . $pmKeyArr[1] . $pmType] = urldecode($pmValues);
+            }
+        }
+
+        return $pmArray;
+    }
 }
