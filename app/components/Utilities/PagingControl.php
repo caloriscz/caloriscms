ca910fceff9ae83e345139ceab79410941be9477
Petr Karásek <caloris@caloris.cz>
2017-11-17 07:52:02 +0100

    pages - paging control missed

---
+++ app/components/Utilities/PagingControl.php
@@ -0,0 +1,19 @@
+<?php
+namespace Caloriscz\Utilities;
+
+use Nette\Application\UI\Control;
+
+class PagingControl extends Control
+{
+
+    public function render($args, $paginator)
+    {
+        $template = $this->template;
+        $template->setFile(__DIR__ . '/PagingControl.latte');
+        $template->args = $args;
+        $template->paginator = $paginator;
+        
+        $template->render();
+    }
+
+}
